e724a26df762a09a0db64289bf888b52269ef7d8
One 'L' of a Girl <132393984+one-L-of-a-girl@users.noreply.github.com>
2023-09-19 15:44:20 -0400

    aligning public and private repos

--- TIM.docker_cleanup.sh
+++ TIM.docker_cleanup.sh
@@ -4,6 +4,10 @@ printf "\nExecuting Docker cleanup.\n"
 # Stop all containers at once
 docker-compose --project-name tim -f TIM.docker-compose.yml --env-file $TIM_ENV_FILE down
 
+if [ $VISIBILITY = "PUBLIC" ]; then
+    docker-compose --project-name tim -f TIM.extractor.docker-compose.yml --env-file $TIM_ENV_FILE down
+fi
+
 # remove TIM locally built images
 docker rmi $(docker images | grep tim | awk '{print $3}') 
 

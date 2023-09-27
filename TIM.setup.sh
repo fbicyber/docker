7410a3c8885343b94c42985a72b625b7d8bbb86a
One 'L' of a Girl <132393984+one-L-of-a-girl@users.noreply.github.com>
2023-09-27 11:01:37 -0400

    wip

--- TIM.setup.sh
+++ TIM.setup.sh
@@ -243,6 +243,7 @@ RUN_DOCKER() {
     if [ $VISIBILITY = "PRIVATE" ]; then
         # extractor is only available in the PRIVATE repo
         # docker-compose --project-name tim -f ./TIM.docker-compose.yml -f TIM.extractor.docker-compose.yml up --force-recreate --build -d 
+        echo "$DC_CMD ./TIM.docker-compose.yml -f TIM.extractor.docker-compose.yml $DC_OPTS"
         $DC_CMD ./TIM.docker-compose.yml -f TIM.extractor.docker-compose.yml $DC_OPTS
     else
         # docker-compose --project-name tim -f ./TIM.docker-compose.yml up --force-recreate --build -d
